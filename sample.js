9e02d05b9d23530de92754c781da0a3ca26cfb6c
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-16 12:13:30 -0700

    understanding observables

--- sample.js
+++ sample.js
@@ -3,16 +3,21 @@ import { Observable } from "rxjs";
 const observer = {
     next: value => console.log('next', value),
     error: error => console.log('error', error),
-    complete:()=>console.log('complete!')
-
+    complete: () => console.log('complete!')
 }
 
 const observable = new Observable(subscriber =>{
     subscriber.next('Hello');
     subscriber.next('World');
     subscriber.complete();
-    subscriber.next('Hello');
+    subscriber.next('Hello'); // will not run
     subscriber.next('World');
 });
 
-observable.subscribe(observer);
+// observable.subscribe(observer);
+// works the same as above
+observable.subscribe(
+    value => console.log('next', value),
+    error => console.log('error', error),
+    () => console.log('complete!')
+);
