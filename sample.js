adfa5e08e00117a2f0d32a1bfc1fdef02860fe0b
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-16 12:22:57 -0700

    deliver values ansyc w/observables

--- sample.js
+++ sample.js
@@ -1,23 +1,31 @@
 import { Observable } from "rxjs";
 
 const observer = {
-    next: value => console.log('next', value),
-    error: error => console.log('error', error),
-    complete: () => console.log('complete!')
-}
+  next: (value) => console.log("next", value),
+  error: (error) => console.log("error", error),
+  complete: () => console.log("complete!"),
+};
 
-const observable = new Observable(subscriber =>{
-    subscriber.next('Hello');
-    subscriber.next('World');
+const observable = new Observable((subscriber) => {
+  let count = 0;
+
+  const id = setInterval(() => {
+    subscriber.next(count);
     subscriber.complete();
-    subscriber.next('Hello'); // will not run
-    subscriber.next('World');
-});
+    count += 1;
+  }, 1000);
 
-// observable.subscribe(observer);
-// works the same as above
-observable.subscribe(
-    value => console.log('next', value),
-    error => console.log('error', error),
-    () => console.log('complete!')
-);
+  return () => {
+    console.log("called");
+    clearInterval(id);
+  };
+});
+console.log("before");
+observable.subscribe(observer);
+console.log("after");
+// // works the same as above
+// observable.subscribe(
+//     value => console.log('next', value),
+//     error => console.log('error', error),
+//     () => console.log('complete!')
+// );
