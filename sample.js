c02baa2a635fa94ca4d80d47fd50c29c28745a8c
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-23 13:28:55 -0700

    regeneratorRuntime issue

--- sample.js
+++ sample.js
@@ -1,5 +1,13 @@
-import { of, range } from "rxjs";
+import { /*of, range, fromEvent,*/ from } from "rxjs";
 
+function* hello() {
+  yield 'Hello';
+  yield 'World';
+}
+
+const iterator = hello();
+console.log(iterator.next().value);
+console.log(iterator.next().value);
 
 const observer = {
   next: val => console.log('next', val),
@@ -7,9 +15,20 @@ const observer = {
   complete: () => console.log('complete!')
 }
 
+
+// const source$ = of(1, 2, 3, 4, 5); 
+// const source$ = from([1, 2, 3, 4, 5]); // one char at a time
+// const source$ = from('Hello!'); // uses index to emit one at a time
+const source$ = from(fetch(
+  'https://api.github.com/users/octocat'
+));
+// source$.subscribe(observer);
+
+
+// of
 // const source$ = of([1], 2, 3, 4, 5);
-const source$ = range(1, 5);
-source$.subscribe(observer);
+// const source$ = range(1, 5);
+// source$.subscribe(observer);
 
 
 // const source$ = fromEvent(document, 'keyup');
