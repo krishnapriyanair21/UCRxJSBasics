d14df21c830625571b24c4a114c71123813debff
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-07-07 11:36:34 -0700

    intro to marble diagrams

--- sample.js
+++ sample.js
@@ -1,7 +1,8 @@
-/* Emit items based on a duration with interval and timer */
-import { interval , timer} from "rxjs";
-
-const timer$ = timer(2000);
-
-timer$.subscribe(console.log);
-
+/* Introduction to marble diagrams */
+// first line is input observable (arrow is timeline)
+// second line/box name of operator + config options passed
+// third line is output observable 
+// "marbles": circles representing coresponding input output values 
+  // on first and third line
+// completion is notified via vertical line
+// error is notified via "x" in corresponding circle
